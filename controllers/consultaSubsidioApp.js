9fd86e6a24f818e471023285e1287ea4b6b8e173
Jefferson Echeverry <echeverryjefferson@gmail.com>
2020-05-15 19:56:01 -0500

    Se incluye los metodos para obtener el orden la listas

--- controllers/consultaSubsidioApp.js
+++ controllers/consultaSubsidioApp.js
@@ -51,7 +51,7 @@ exports.findDateSubsidios = (req, res) => {
                     var contador  = 0;
                     console.log("La fecha inicio es ", dateInit);
                     console.log("La fecha final es ", dateFinal);
-                    
+
                     for(var i = 0; i < subsidio.length; i++){
                         console.log("En la posicion i el subsidio es ", subsidio[i]);
                         console.log("La fecha es ", subsidio[i].fecha);
@@ -63,8 +63,10 @@ exports.findDateSubsidios = (req, res) => {
                             contador++;
                         }
                     }
-                                        
-                    res.status(codigoHttp.respuestaExitosa).json({'codigoRetorno': codigoRetorno.codigoExito, 'mensaje': mensajeRetorno.mensajeExito, 'body': subsidioResult}); //
+                    
+                    const resultado = getListasObjetosFechas(subsidioResult);
+
+                    res.status(codigoHttp.respuestaExitosa).json({'codigoRetorno': codigoRetorno.codigoExito, 'mensaje': mensajeRetorno.mensajeExito, 'body': resultado});
                 }else{
                     res.status(codigoHttp.respuestaExitosa).json({'codigoRetorno': codigoRetorno.codigoFallido, 'mensaje': mensajeRetorno.mensajeFallido, 'body': 'No hay subsidios almacenados'});
                 }
@@ -73,3 +75,65 @@ exports.findDateSubsidios = (req, res) => {
         }
     });
 }
+
+function getCantidadFechas(subsidioResult) {
+    console.log("Entro al metodo getCantidadFechas");
+    var fechas = [];
+    var contador = 0;
+    console.log("El tamaño de subsidio es ",subsidioResult.length);
+    for(var i = 0; i < subsidioResult.length; i++) {
+        var fecha = subsidioResult[i].fecha;
+        console.log("La fecha es "+fecha);
+        var fechaString =`${fecha.getDate()}/${fecha.getMonth()}/${fecha.getFullYear()}`;
+        console.log("La fechaString es ",fechaString);
+        if(i == 0) {
+            console.log("Entro en la posicion 0 y la posicion es ", fechaString);
+            fechas[contador] = fechaString;
+            contador++;
+        } else if(!fechas.includes(fechaString)) {
+            console.log("Entro en el metodo includes fecha y la fecha es ",fechaString);
+            fechas[contador] = fechaString;
+            contador++;
+        }
+    }
+    console.log("Finalizo el metodo getCantidadFechas");
+    return fechas;
+}
+
+function getListasFechas(subsidioResult) {  
+    console.log("Entro al metodo getListasFechas");
+    var fechas = getCantidadFechas(subsidioResult);
+    var data = [];
+    var listas = [];
+    console.log("El tamaño de fechas es ", fechas.length);
+    for(var i = 0; i < fechas.length; i++) {
+        console.log("La fecha para obtener la cantidad de fechas es ",fecha[i]);
+        data = subsidioResult.filter(element => `${element.getDate()}/${element.getMonth()}/${element.getFullYear()}` == fechas[i]);
+        data.forEach(element => console.log("El elemento en el foreach es ", element));
+        listas[i] = data;
+    }
+    console.log("Finalizo el metodo getListasFechas");
+    return listas;
+}
+
+function getListasObjetosFechas(subsidioResult) {
+    console.log("Entro al metodo getListasObjetosFechas");
+    var resultado = [];
+    var listas = getListasFechas(subsidioResult);
+    console.log("El tamaño de listas es ",listas.length);
+    for(var i = 0; i < listas.length; i++) {
+        var lista = listas[i];
+        var objeto = {
+            fecha: lista[0],
+            cantidad: lista.length
+        };
+        console.log("La fecha del objeto es ", objeto.fecha);
+        console.log("La cantidad es ", objeto.cantidad);
+        resultado[i] = objeto;
+    }
+
+    return resultado;
+}
+
+
+
